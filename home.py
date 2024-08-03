b2e5e9f089af6ffff70c63f6ed547278ed93df87
umeshkmrgithub <umeshkmr@gmail.com>
2024-08-03 22:32:51 +0300

    v2.2

---
+++ home.py
@@ -0,0 +1,26 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+# import home, trending, account, your, about
+
+
+
+st.set_page_config(
+    page_title="TRANSACTION BASED ANALYTICS ",
+)
+
+st.title("Data Analytics on PhonePe dataset")
+st.sidebar.success('Select a page above.')
+
+st.sidebar.image("images/umesh_profile_pic.jpg", use_column_width=True)
+
+
+'''
+#### PROJECT 2 
+'''
+
+
+'**Name:** R. Umesh Kumar'
+'**Batch Code:** MD108'
+'**Course:** Data Science Master Program by Guvi.in'
+'**Github link:** https://github.com/umeshkmrgithub/2_DS_ANALYTICS_APP.git '
+'**Email:** umeshkmr@gmail.com '
